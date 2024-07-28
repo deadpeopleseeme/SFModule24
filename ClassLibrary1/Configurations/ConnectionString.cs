2ebee473865378d33811140c3f970378c0b5d6b9
Eddy <volkikushaytnogi@gmail.com>
2024-07-28 16:10:28 +0200

    replaced .net framework with .net

---
+++ ClassLibrary1/Configurations/ConnectionString.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary.Configurations
+{
+    public static class ConnectionString
+    {
+        public static string MsSqlConnection => @"Data Source=.\SQLEXPRESS;Database=testing;Trusted_Connection=True;";
+    }
+}
